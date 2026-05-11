203d6379def2f5ae4512a76cfb13f841b9b622de
st10056037 <Student@LabVM2049826>
2026-05-11 10:01:43 +0200

    initial migration

--- GLMS.Web/Data/Migrations/20260511075740_TechMove_GLMS.Designer.cs
+++ GLMS.Web/Data/Migrations/20260511075740_TechMove_GLMS.Designer.cs
@@ -4,6 +4,7 @@ using GLMS.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GLMS.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260511075740_TechMove_GLMS")]
+    partial class TechMove_GLMS
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
